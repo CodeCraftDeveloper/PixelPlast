3031edeea82d9c5cca62ef41831173161c66deed
chandi977 <pappumahato000@gmail.com>
2026-09-04 17:26:04 +0530

    fix: update amplify.yml baseDirectory and remove customHttp.yml, adjust next.config.mjs settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
   trailingSlash: true,
-  allowedDevOrigins: [
-    "192.168.5.233",
-    "192.168.*.*",
-    "localhost",
-    "127.0.0.1",
-  ],
 };
 
 export default nextConfig;
